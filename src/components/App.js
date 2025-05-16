cbc737200024e63c312cce0e21a58a3c774e3fb5
ashish <ashishmasih92g@gmail.com>
2025-05-16 11:54:01 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,74 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [inputList, setInputList] = useState([{ name: "", age: "" }])
+  let [inputval, setInputval] = useState([{ name: "", age: "" }])
+
+  function changeInput(e, index) {
+    let { name, value } = e.target;
+
+    let list = [...inputList]
+    list[index][name] = value
+    setInputList(list)
+  }
+
+
+
+
+  console.log(inputList);
+
+
+function removeInput(index){
+
+  let list = [...inputList]
+
+  list.splice(index,1)
+
+  setInputList(list)
+
+}
+
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {
+        inputList.map((v, i) => {
+          return (
+            <>
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  onChange={(e) => changeInput(e,i)}
+                  value={v.name}
+                />
+                <input
+                  type="number"
+                  name="age"
+                  onChange={(e) => changeInput(e,i)}
+                  value={v.age}
+                />
+                <button onClick={()=>{  
+                  removeInput(i)
+                }}  >Remove</button>
+              </div>
+            </>
+          )
+        })
+      }
+      <button onClick={(e) => {
+        setInputList([...inputList, { name: "", age: "" }])
+      }} >Add more</button>
+      <button onClick={(e)=>{
+          e.stopPropagation()
+      console.log("Submit Data ",inputList);
+      
+      }}>Submit</button>
+
     </div>
   )
 }
